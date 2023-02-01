b28e43209431f56f9287ba1150d0f929b597ff1d
adityashrivastav1 <aditya.shri9981@gmail.com>
2023-02-01 23:20:57 +0530

    generalized the comp-> Pet with props

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ function App() {
     {},
     [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
+      React.createElement(Pet, {
+        name: "Luna",
+        breed: "Havanese",
+        animal: "Dog",
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        breed: "Cockatiel",
+        animal: "Bird",
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        breed: "Mixed",
+        animal: "Cat",
+      }),
     ])
 }
 
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ])
 }
 
