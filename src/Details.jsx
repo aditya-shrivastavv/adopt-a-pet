dea7186930d5fb3ba8e3cc1c12e26b439e9ef1a8
adityashrivastav1 <aditya.shri9981@gmail.com>
2023-02-05 17:19:33 +0530

    introduced class component > Carousel.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
 import fetchPets from "./fetchPets";
+import Carousel from "./Carousel";
 
 export default function Details() {
   const { id } = useParams();
@@ -19,6 +20,7 @@ export default function Details() {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
