d252f0af74d378a01ef392bc86fb21936560d766
adityashrivastav1 <aditya.shri9981@gmail.com>
2023-02-09 11:33:56 +0530

    dist path added in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src", // location of index.html
+  build: {
+    outDir: "../dist",
+  },
 });
